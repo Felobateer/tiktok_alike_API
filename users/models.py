ab92d60d23823a938813d02c921375d2824d33be
Felobateer <felobateersamy01@gmail.com>
2025-01-18 03:52:12 +0100

    update migraitons

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 # Create your models here.
 class User(AbstractUser):
@@ -7,6 +7,21 @@ class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name="custom_user_set",  # Add a unique related_name
+        blank=True,
+        help_text="The groups this user belongs to.",
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name="custom_user_set",  # Add a unique related_name
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
 
     def __str__(self):
         return self.username
