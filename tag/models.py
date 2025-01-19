7d38e682ada646b432c8129ca70490d7c41c4e63
Felobateer <felobateersamy01@gmail.com>
2025-01-19 08:29:57 +0100

    add tag model

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']  # Order tags alphabetically
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    def __str__(self):
+        return self.name
